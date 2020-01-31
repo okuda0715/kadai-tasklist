2b3c090a6247d14f97b3429813ff32612429e0be
yuki.okuda <okuda.y0715@gmail.com>
2020-01-31 13:32:06 +0000

    status

--- database/migrations/2020_01_31_214952_add_title_to_tasks_table.php
+++ database/migrations/2020_01_31_214952_add_title_to_tasks_table.php
@@ -13,7 +13,7 @@ class AddTitleToTasksTable extends Migration
      */
     public function up()
     {
-        Schema::table('status', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $table) {
             $table->string('status');
         });
     }
@@ -25,7 +25,7 @@ class AddTitleToTasksTable extends Migration
      */
     public function down()
     {
-        Schema::table('status', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $table) {
             $table->dropColumn('status');
         });
     }
